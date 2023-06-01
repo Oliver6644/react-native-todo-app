ae3a6cf44a914c4c8f06257cddca94e0651dd261
Oliver Tkac <olivertkac6644@gmail.com>
2023-06-01 23:30:48 +0200

    ep .14 Flexbox Basics

--- App.js
+++ App.js
@@ -11,6 +11,7 @@ import {
 import Header from "./components/header";
 import TodoItem from "./components/todoItem";
 import AddTodo from "./components/addTodo";
+import Sandbox from "./components/sandbox";
 
 export default function App() {
   const [todos, setTodos] = useState([
@@ -38,6 +39,7 @@ export default function App() {
   };
 
   return (
+    //<Sandbox />
     <TouchableWithoutFeedback
       onPress={() => {
         Keyboard.dismiss();
@@ -70,8 +72,10 @@ const styles = StyleSheet.create({
   content: {
     paddingTop: 40,
     paddingLeft: 20,
+    flex: 1,
   },
   list: {
+    flex: 1,
     marginTop: 20,
   },
 });
